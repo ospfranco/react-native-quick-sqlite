e99e0b6df45a3af0a72c8f5a1282c95a4eb8e4f7
Oscar Franco <ospfranco@protonmail.com>
2021-02-27 13:32:00 +0100

    Working opening of database! YAY!

--- cpp/sequel.cpp
+++ cpp/sequel.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <sstream>
 #include <sqlite3.h>
 
 using namespace std;
 
 bool sequel_open(string const& dbName) {
-  cout << "Trying to open db connection" << endl;
+  cout << "react-native-sequel: Trying to open db connection" << endl;
+  char *home = getenv("HOME");
+  char *subdir = "/Documents/";
+  
+  stringstream ss;
+  ss << home << subdir << dbName;
+  string dbPath = ss.str();
+
+  // s.erase(0, strlen("/private")); // Erase “/Private” from the final string
+
   int sqlOpenFlags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
 
   sqlite3 *db;
 
   int exit = 0;
-  exit = sqlite3_open_v2(dbName.c_str(), &db, sqlOpenFlags, nullptr);
+  exit = sqlite3_open_v2(dbPath.c_str(), &db, sqlOpenFlags, nullptr);
 
   if(exit != SQLITE_OK) {
-    cout << "Error opening database: " << sqlite3_errmsg(db) << endl;
+    cout << "react-native-sequel: Error opening database [" << dbName << "]: " << sqlite3_errmsg(db) << endl;
     return false;
   } else {
     cout << "Opened database successfully!" << endl;
