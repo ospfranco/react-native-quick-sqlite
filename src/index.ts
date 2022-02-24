9eff9a86d9ba6c27a0e73ff12f94ed0ce5f0384e
Oscar Franco <ospfranco@protonmail.com>
2022-02-24 09:34:48 +0100

    Update index.ts with better comments and fix item function assignment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
-// IMPORTANT!!!!!!!!!!!
-// JSI BINDINGS DO NOT WORK WHEN CONNECTED TO THE DEBUGGER
-// This is a low level api
+/**
+ * JSI BINDINGS DO NOT WORK WHEN CONNECTED TO THE CHROME DEBUGGER
+ * Use flipper to debug your RN apps from now on
+ */
+
+/**
+ * Object returned by SQL Query executions {
+ *  status: 0 or undefined for correct execution, 1 for error
+ *  insertId: Represent the auto-generated row id if applicable
+ *  rowsAffected: Number of affected rows if result of a update query
+ *  message: if status === 1, here you will find error description
+ *  rows: if status is undefined or 0 this object will contain the query results
+ * }
+ *
+ * @interface QueryResult
+ */
+interface QueryResult {
+  status?: 0 | 1;
+  insertId?: number;
+  rowsAffected: number;
+  message?: string;
+  rows?: {
+    /** Raw array with all dataset */
+    _array: any[];
+    /** The lengh of the dataset */
+    length: number;
+    /** A convenience function to acess the index based the row object
+     * @param idx the row index
+     * @returns the row structure identified by column names
+     */
+    item: (idx: number) => any;
+  };
+}
 interface ISQLite {
   open: (dbName: string, location?: string) => any;
   close: (dbName: string) => any;
@@ -8,21 +38,11 @@ interface ISQLite {
     dbName: string,
     query: string,
     params: any[] | undefined
-  ) => {
-    rows: {
-      _array: any[];
-      length: number;
-      item: (idx: number) => any;
-    };
-    insertId?: number;
-    rowsAffected: number;
-  };
+  ) => QueryResult;
   // backgroundExecuteSql: (dbName: string, query: string, params: any[]) => any;
 }
 
-declare global {
-  var sqlite: ISQLite;
-}
+declare var sqlite: ISQLite;
 
 // API FOR TYPEORM
 interface IConnectionOptions {
@@ -34,7 +54,7 @@ interface IDBConnection {
   executeSql: (
     sql: string,
     args: any[],
-    ok: (res: any) => void,
+    ok: (res: QueryResult) => void,
     fail: (msg: string) => void
   ) => void;
   close: (ok: (res: any) => void, fail: (msg: string) => void) => void;
@@ -52,14 +72,17 @@ export const openDatabase = (
       executeSql: (
         sql: string,
         params: any[] | undefined,
-        ok: any,
-        fail: any
+        ok: (res: QueryResult) => void,
+        fail: (msg: string) => void
       ) => {
         try {
           // console.warn(`[react-native-quick-sqlite], sql: `, sql, ` params: ` , params);
           let response = sqlite.executeSql(options.name, sql, params);
-          // enhance object to allow the sqlite-storage typeorm driver to work
-          response.rows.item = (idx: number) => response.rows._array[idx];
+
+          // Add 'item' function to result object to allow the sqlite-storage typeorm driver to work
+          if (response.rows != null) {
+            response.rows.item = (idx: number) => response.rows._array[idx];
+          }
 
           ok(response);
         } catch (e) {
