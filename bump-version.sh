1590c9d29b728760f65662826d981cf1d8a06b98
Oscar Franco <ospfranco@protonmail.com>
2022-07-07 07:56:26 +0200

    Fix bump version script to include PACKAGE_VERSION variable

--- bump-version.sh
+++ bump-version.sh
@@ -8,6 +8,8 @@ git add .
 
 git commit -m "Bump version"
 
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+
 git tag $PACKAGE_VERSION
 
 git push
