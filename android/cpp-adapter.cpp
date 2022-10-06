9b9acd4416530a1e9c74c2b2d4d14beac8696fa3
Oscar Franco <ospfranco@protonmail.com>
2022-10-06 14:08:47 -0400

    Fix android compilation

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -1,37 +1,35 @@
-#include <jni.h>
-#include <fbjni/fbjni.h>
-#include <jsi/jsi.h>
-#include <ReactCommon/CallInvokerHolder.h>
-#include "installer.h"
+#include "bindings.h"
 #include "logs.h"
+#include <ReactCommon/CallInvokerHolder.h>
+#include <fbjni/fbjni.h>
+#include <jni.h>
+#include <jsi/jsi.h>
 #include <typeinfo>
 
-struct QuickSQLiteBridge : jni::JavaClass<QuickSQLiteBridge>
-{
-  static constexpr auto kJavaDescriptor = "Lcom/reactnativequicksqlite/QuickSQLiteBridge;";
+struct QuickSQLiteBridge : jni::JavaClass<QuickSQLiteBridge> {
+  static constexpr auto kJavaDescriptor =
+      "Lcom/reactnativequicksqlite/QuickSQLiteBridge;";
 
-  static void registerNatives()
-  {
-    javaClassStatic()->registerNatives({// initialization for JSI
-                                        makeNativeMethod("installNativeJsi", QuickSQLiteBridge::installNativeJsi)});
+  static void registerNatives() {
+    javaClassStatic()->registerNatives(
+        {// initialization for JSI
+         makeNativeMethod("installNativeJsi",
+                          QuickSQLiteBridge::installNativeJsi)});
   }
 
 private:
-  static void installNativeJsi(jni::alias_ref<jni::JObject> thiz,
-                               jlong jsiRuntimePtr,
-                               jni::alias_ref<react::CallInvokerHolder::javaobject> jsCallInvokerHolder,
-                               jni::alias_ref<jni::JString> docPath)
-  {
+  static void installNativeJsi(
+      jni::alias_ref<jni::JObject> thiz, jlong jsiRuntimePtr,
+      jni::alias_ref<react::CallInvokerHolder::javaobject> jsCallInvokerHolder,
+      jni::alias_ref<jni::JString> docPath) {
     auto jsiRuntime = reinterpret_cast<jsi::Runtime *>(jsiRuntimePtr);
     auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
     std::string docPathString = docPath->toStdString();
 
-    install(*jsiRuntime, jsCallInvoker, docPathString.c_str());
+    osp::install(*jsiRuntime, jsCallInvoker, docPathString.c_str());
   }
 };
 
-JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *)
-{
-  return jni::initialize(vm, []
-                         { QuickSQLiteBridge::registerNatives(); });
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *) {
+  return jni::initialize(vm, [] { QuickSQLiteBridge::registerNatives(); });
 }
